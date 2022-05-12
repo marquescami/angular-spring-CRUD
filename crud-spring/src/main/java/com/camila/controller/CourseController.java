a2e41acee3fd46f6d0d9cfca71cf846a9b89711e
marquescami <camilamsc@hotmail.com>
2022-05-12 19:58:20 -0300

    criando curso na API Spring

--- crud-spring/src/main/java/com/camila/controller/CourseController.java
+++ crud-spring/src/main/java/com/camila/controller/CourseController.java
@@ -1,8 +1,13 @@
 package com.camila.controller;
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.camila.model.Course;
@@ -23,5 +28,13 @@ public class CourseController {
     public List<Course> list() {
         return courseRepository.findAll();
     }
-
+    
+    @PostMapping
+    @ResponseStatus(code = HttpStatus.CREATED)
+    public Course create(@RequestBody Course course) {
+        // System.out.println(course.getName());
+        return courseRepository.save(course);
+      //  return ResponseEntity.status(HttpStatus.CREATED)
+            //.body(courseRepository.save(course));
+    }
 }
